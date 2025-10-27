f7ef83940fbcf1f3b0f4381f859eae7a3796ab44
Travis <119350720+Tmino1@users.noreply.github.com>
2025-10-27 13:00:35 -0500

    implemented basic main menu, added button sprites and placeholder akinator

--- src/util/constants.py
+++ src/util/constants.py
@@ -1,3 +1,17 @@
+import arcade
+from arcade.gui import UIFlatButton, UIManager, UIAnchorLayout
+import pathlib
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 WINDOW_TITLE = "Brother Akinator"
+
+transparent_style = {
+            'normal'   : UIFlatButton.UIStyle(bg=[0,0,0,0]),
+            'hover'    : UIFlatButton.UIStyle(bg=[0,0,0,0]),
+            'press'    : UIFlatButton.UIStyle(bg=[0,0,0,0]),
+            'disabled' : UIFlatButton.UIStyle(bg=[0,0,0,0])
+            }
+
+main_men_buttons = arcade.load_texture(pathlib.Path(r"src\sprites\buttons.png"))
+akinator_1 = arcade.load_texture(pathlib.Path(r"src\sprites\akinator_1.jpg"))
--- src/views/main_menu.py
+++ src/views/main_menu.py
@@ -1,17 +1,84 @@
 import arcade
+import util.constants as constants
 from util.constants import WINDOW_HEIGHT, WINDOW_WIDTH
+from arcade.gui import (
+    UIManager,
+    UITextureButton,
+    UIAnchorLayout,
+    UIView,
+    UIFlatButton
+)
+
 
 class MenuView(arcade.View):
     """ Class that manages the 'menu' view. """
+    def __init__(self):
+        super().__init__()
 
+        # Create a UIManager
+        self.ui = UIManager()
+
+        # Create an anchor layout, which can be used to position widgets on screen
+        anchor = self.ui.add(UIAnchorLayout())
+
+        self.buttons = arcade.Sprite(constants.main_men_buttons)
+        self.buttons.scale = .5
+        self.buttons.center_x = 3*WINDOW_WIDTH /4 
+        self.buttons.center_y = WINDOW_HEIGHT /4 + 40
+
+        self.akinator = arcade.Sprite(constants.akinator_1)
+        self.akinator.scale = 1
+        self.akinator.center_x = WINDOW_WIDTH /4 
+        self.akinator.center_y = WINDOW_HEIGHT /2
+
+        # Add a button switch to the other View.
+        start_button = anchor.add(
+            UIFlatButton(width=340,height=100, style=constants.transparent_style),
+            align_y=1,
+            align_x=WINDOW_WIDTH/4
+
+        )
+        settings_button = anchor.add(
+            UIFlatButton(width=340,height=100, style=constants.transparent_style),
+            align_y=-135,
+            align_x=WINDOW_WIDTH/4
+        )
+        exit_button = anchor.add(
+            UIFlatButton(width=340,height=100, style=constants.transparent_style),
+            align_y=-275,
+            align_x=WINDOW_WIDTH/4
+        )
+
+        @start_button.event("on_click")
+        def on_click(event):
+            print("DEBUG: moving to questions")
+            #self.window.show_view(BlueView())
+
+        @settings_button.event("on_click")
+        def on_click(event):
+            print("DEBUG: moving to settings")
+            #self.window.show_view(BlueView())
+
+        @exit_button.event("on_click")
+        def on_click(event):
+            arcade.exit()
+
+            
     def on_show_view(self):
         """ Called when switching to this view"""
         self.window.background_color = arcade.color.BLACK
+        self.ui.enable()
+    
+    def on_hide_view(self) -> None:
+        self.ui.disable()
 
     def on_draw(self):
         """ Draw the menu """
         self.clear()
-        arcade.draw_text("Main Menu", WINDOW_WIDTH / 2, WINDOW_HEIGHT - 100,
+        arcade.draw_sprite(self.buttons)
+        arcade.draw_sprite(self.akinator)
+        self.ui.draw()
+        arcade.draw_text("BROTHER AKINATOR", 3*WINDOW_WIDTH / 4, WINDOW_HEIGHT - 100,
                          arcade.color.GOLD, font_size=30, anchor_x="center")
 
    
