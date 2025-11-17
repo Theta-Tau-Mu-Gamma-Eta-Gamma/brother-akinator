c13d991989f9bf05173c74006be6c26c679d3568
Travis <119350720+Tmino1@users.noreply.github.com>
2025-11-17 10:00:10 -0500

    csv handling completed

--- src/window.py
+++ src/window.py
@@ -1,5 +1,21 @@
 import arcade
+import util.akinator_mnb as mnb
+import copy
+import numpy as np
 
 class BrotherWindow(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
+
+        # uncomment this line when ready to use the model, 
+        # it creates a deepcopy so that when using the model for questions it doenst get messed up
+        model = mnb.get_model("dataset-v1.csv")
+        
+        #used to keep track of answers
+        user_answers = np.full((1, mnb.X.shape[1]), np.nan)
+        asked_questions = set()
+
+        
+        
+
+        
